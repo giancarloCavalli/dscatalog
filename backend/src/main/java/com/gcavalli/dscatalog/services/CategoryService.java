ec4027cf2e42fc41dba8e22644786acb5efa680d
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-05-26 21:55:09 -0300

    Database seeding

--- backend/src/main/java/com/gcavalli/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gcavalli/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gcavalli.dscatalog.entities.Category;
 import com.gcavalli.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repo;
 	
+	//transactional garante que vai executar completamente ou nada. ReadOnly aumenta a performance dos selects (não da locking em operações de leitura).
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repo.findAll();
 	}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (name) VALUES ('Games');
+INSERT INTO categories (name) VALUES ('Books');
+INSERT INTO categories (name) VALUES ('Clothing');
+INSERT INTO categories (name) VALUES ('Surfing');
