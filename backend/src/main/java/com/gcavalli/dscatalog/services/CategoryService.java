b8ae279710dd5312b778a3f63fe9f8e2d472ebde
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-05-26 22:13:25 -0300

    CategoryDTO

--- backend/src/main/java/com/gcavalli/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gcavalli/dscatalog/services/CategoryService.java
@@ -1,12 +1,13 @@
 package com.gcavalli.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.gcavalli.dscatalog.entities.Category;
+import com.gcavalli.dscatalog.dto.CategoryDTO;
 import com.gcavalli.dscatalog.repositories.CategoryRepository;
 
 @Service
@@ -17,8 +18,9 @@ public class CategoryService {
 	
 	//transactional garante que vai executar completamente ou nada. ReadOnly aumenta a performance dos selects (não da locking em operações de leitura).
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repo.findAll();
+	public List<CategoryDTO> findAll() {
+		return repo.findAll()
+				.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 }
