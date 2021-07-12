0237e8bc7f8b825bbee85547b0c7ea5cfe7a3324
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-07-12 19:20:43 -0300

    ORM mapping, database seeding

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,13 @@
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Alex', 'Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Maria', 'Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO roles (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO roles (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 1);
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
+
 INSERT INTO categories (name, created_At) VALUES ('Livros', NOW());
 INSERT INTO categories (name, created_At) VALUES ('Eletrônicos', NOW());
 INSERT INTO categories (name, created_At) VALUES ('Computadores', NOW());
