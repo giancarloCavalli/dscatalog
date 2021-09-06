d96b09ba658ff65b675765043913de6a9ff4db10
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-09-06 07:01:41 -0300

    Auth Routes

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -4,6 +4,7 @@ import Navbar from "components/Navbar";
 import Catalog from "pages/Catalog";
 import Admin from "pages/Admin";
 import ProductDetails from "pages/ProductDetails";
+import Auth from "pages/Admin/Auth";
 
 
 const Routes = () => (
@@ -18,6 +19,10 @@ const Routes = () => (
         </Route>
         <Route path="/products/:productId">
           <ProductDetails />
+        </Route>
+        <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+        <Route path="/admin/auth">
+          <Auth />
         </Route>
         <Redirect from="/admin" to="/admin/products" exact />
         <Route path="/admin">
