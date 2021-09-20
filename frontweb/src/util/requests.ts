c5a8836546ab102f0034bb26cb910c9682ff9a8d
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-09-20 06:49:04 -0300

    Function hasAnyRoles

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -100,3 +100,18 @@ export const isAuthenticated = (): boolean => {
   const tokenData = getTokenData();
   return (tokenData && tokenData?.exp > (Date.now() / 1000)) ? true : false;
 }
+
+export const hasAnyRoles = (roles: Role[]): boolean => {
+  if (roles.length === 0)
+    return true;
+
+  const tokenData = getTokenData();
+
+  if (tokenData !== undefined) {
+    for (let i = 0; i < roles.length; i++) {
+      if (tokenData.authorities.includes(roles[i]))
+        return true;
+    }
+  }
+  return false;
+}
