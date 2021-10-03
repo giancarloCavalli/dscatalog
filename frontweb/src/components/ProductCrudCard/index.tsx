69b5406e329be858096184e21d7d6f7dbabd42ea
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-10-03 09:33:00 -0300

    ProductList basic layout

---
+++ frontweb/src/components/ProductCrudCard/index.tsx
@@ -0,0 +1,24 @@
+import ProductPrice from 'components/ProductPrice';
+import { Product } from 'types/product';
+
+import './styles.css';
+
+type Props = {
+  product: Product;
+}
+
+const ProductCrudCard = ({ product }: Props) => {
+  return (
+    <div className="base-card product-card">
+      <div className="card-top-container">
+        <img src={product.imgUrl} alt={product.name} />
+      </div>
+      <div className="card-bottom-container">
+        <h5>{product.name}</h5>
+        <ProductPrice price={product.price} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductCrudCard;
