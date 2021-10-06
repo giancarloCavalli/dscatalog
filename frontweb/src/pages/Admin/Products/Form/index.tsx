a161a30edb2d190b0a6de3aadf77fbda539ed5bc
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-10-06 19:51:14 -0300

    Product Crud Card form buttons styling

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -3,25 +3,35 @@ import './styles.css';
 const Form = () => {
   return (
     <div className="base-card product-crud-form-card-container">
-      <div>
+      <div className="product-crud-form-title">
         <h3>Dados do produto</h3>
       </div>
 
       <form action="">
-        <div className="row">
+        <div className="row product-crud-form-inputs-container">
           <div className="col-lg-6">
-            <input type="text" className="form-control base-input" />
-            <input type="text" className="form-control base-input" />
-            <input type="text" className="form-control base-input" />
+            <div className="product-crud-form-input">
+              <input type="text" className="form-control base-input" />
+            </div>
+            <div className="product-crud-form-input">
+              <input type="text" className="form-control base-input" />
+            </div>
+            <div className="product-crud-form-input product-crud-form-input-last-item">
+              <input type="text" className="form-control base-input" />
+            </div>
           </div>
           <div className="col-lg-6">
-            <textarea name="" rows={10} className="form-control base-input"></textarea>
+            <textarea
+              name=""
+              rows={10}
+              className="form-control base-input h-auto"
+            ></textarea>
           </div>
         </div>
 
-        <div>
-          <button className="btn btn-outline-danger">CANCELAR</button>
-          <button className="btn btn-primary">SALVAR</button>
+        <div className="product-crud-form-buttons-container">
+          <button className="btn btn-outline-danger product-crud-btn">CANCELAR</button>
+          <button className="btn btn-primary product-crud-btn">SALVAR</button>
         </div>
       </form>
     </div>
