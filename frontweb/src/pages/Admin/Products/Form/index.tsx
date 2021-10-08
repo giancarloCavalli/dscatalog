a09aee5529b9a3c70e699d451534b8bb0317b792
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-10-08 06:48:54 -0300

    Product CRUD routes

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -30,6 +30,7 @@ const Form = () => {
     requestBackend(config)
       .then((response) => {
         console.log(response.data);
+        history.push("/admin/products");
       })
       .catch((error) => {
         console.log(error);
--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -1,4 +1,5 @@
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 import CategoryBadge from '../CategoryBadge';
 
@@ -26,12 +27,16 @@ const ProductCrudCard = ({ product }: Props) => {
         </div>
       </div>
       <div className="product-crud-card-buttons-container">
-        <button className="btn btn-outline-danger product-crud-card-button">
-          EXCLUIR
-        </button>
-        <button className="btn btn-outline-secondary product-crud-card-button">
-          EDITAR
-        </button>
+        <Link to={`/`}>
+          <button className="btn btn-outline-danger product-crud-card-button">
+            EXCLUIR
+          </button>
+        </Link>
+        <Link to={`/admin/products/${product.id}`}>
+          <button className="btn btn-outline-secondary product-crud-card-button">
+            EDITAR
+          </button>
+        </Link>
       </div>
     </div>
   );
