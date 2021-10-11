920110fec71862e9155774cb1baf123ec08f6e25
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-10-11 19:17:38 -0300

    ProductFilter React Hook Form

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -1,23 +1,74 @@
 import { ReactComponent as SearchIcon } from 'assets/images/search-icon.svg';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import Select from 'react-select';
+import { Category } from 'types/category';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
+type ProductFilterData = {
+  name: string;
+  category: Category;
+};
+
 const ProductFilter = () => {
+
+  const [selectCategories, setSelectCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    requestBackend({ url: '/categories' })
+      .then((response) => {
+        const page = response.data as SpringPage<Category>;
+        setSelectCategories(page.content);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const {
+    register,
+    handleSubmit,
+    control
+  } = useForm<ProductFilterData>();
+
+  const onSubmit = (formData: ProductFilterData) => {
+    console.log('ENVIOU',formData);
+  };
+
   return (
     <div className="base-card product-filter-container">
-      <form action="" className="product-filter-form">
+      <form onSubmit={handleSubmit(onSubmit)} className="product-filter-form">
         <div className="product-filter-name-container">
           <input
+            {...register('name')}
             type="text"
             className="form-control"
             placeholder="Nome do produto"
+            name="name"
           />
-          <SearchIcon />
+          <button>
+            <SearchIcon />
+          </button>
         </div>
         <div className="product-filter-bottom-container">
           <div className="product-filter-category-container">
-            <select>
-              <option value="">Livros</option>
-            </select>
+          <Controller
+                name="category"
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={selectCategories}
+                    isClearable
+                    isSearchable
+                    classNamePrefix="product-crud-form-select"
+                    getOptionLabel={(category: Category) => category.name}
+                    getOptionValue={(category: Category) => String(category.id)}
+                  />
+                )}
+              />
           </div>
           <button className="btn btn-outline-secondary">LIMPAR FILTRO</button>
         </div>
