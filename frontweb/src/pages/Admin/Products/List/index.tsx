0faa77c4117c3ffe7a91a96cade884353c3597ec
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-10-11 06:57:30 -0300

    ProductFilter name input layout

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import Pagination from 'components/Pagination';
+import ProductFilter from 'components/ProductFilter';
 import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -48,7 +49,7 @@ const List = () => {
         <Link to="/admin/products/create">
           <button className="btn btn-primary btn-crud-add">ADICIONAR</button>
         </Link>
-        <div className="base-card product-filter-container">Search bar</div>
+        <ProductFilter />
       </div>
 
       <div className="row">
