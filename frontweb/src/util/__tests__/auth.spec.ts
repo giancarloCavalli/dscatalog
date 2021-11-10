0a6b5585d791f31e3d4f0ab1c91aff4c69da88f8
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-11-09 21:58:04 -0300

    first hasAnyRole test

---
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,10 @@
+import { hasAnyRoles } from '../auth';
+
+describe('hasAnyRoles tests', () => {
+
+  test('should return true when empty list', () => {
+    const result = hasAnyRoles([]);
+    expect(result).toEqual(true);
+  });
+
+});
