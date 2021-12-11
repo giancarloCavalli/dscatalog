339d3056307f591eeccf30776099d3d0adf96ce8
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-12-11 11:01:39 -0300

    Mock Catalog request, watchPathIgnorePatterns setting

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,6 +2,12 @@ import { render, screen, waitFor } from '@testing-library/react';
 import Catalog from '..';
 import { Router } from 'react-router-dom';
 import history from 'util/history';
+import { server } from './helpers/fixtures';
+
+//TODO fix cleanup function need warning
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers()); // Garantee that all the resources will be reseted after each test runs, so the next won't be influenced by the prior
+afterAll(() => server.close());
 
 describe("Catalog tests", () => {
 
