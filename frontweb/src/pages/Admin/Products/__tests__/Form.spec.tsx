286533c44374801f68f19b2bd7c373de1ba72cc6
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-12-26 09:57:39 -0300

    Product form update tests

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event';
 import { Router, useParams } from 'react-router-dom';
 import history from 'util/history';
 import Form from '../Form';
-import { server } from './helpers/fixtures';
+import { productResponse, server } from './helpers/fixtures';
 import selectEvent from 'react-select-event';
 import { ToastContainer } from 'react-toastify';
 
@@ -43,15 +43,15 @@ describe("Product form create tests", () => {
         <Form />
       </Router>
     )
-    
+
     const nameInput = screen.getByTestId("name");
     const priceInput = screen.getByTestId("price");
     const imgUrlInput = screen.getByTestId("imgUrl");
     const descriptionInput = screen.getByTestId("description");
     const categoriesInput = screen.getByLabelText("Categorias");
-    
+
     const submitButton = screen.getByRole('button', { name: /salvar/i }); // Best actual practice to get a button in a form
-    
+
     await selectEvent.select(categoriesInput, ["Eletrônicos", "Computadores"]);
     userEvent.type(nameInput, "Mouse Razer");
     userEvent.type(priceInput, "277,90");
@@ -59,13 +59,13 @@ describe("Product form create tests", () => {
     userEvent.type(descriptionInput, "Mouse braaabo");
 
     userEvent.click(submitButton);
-    
+
     await waitFor(() => {
       expect(screen.getByText("Produto cadastrado com sucesso")).toBeInTheDocument();
     });
 
     expect(history.location.pathname).toEqual('/admin/products');
-    
+
   });
 
   test("should show 5 validation messagens when just clicking submit", async () => {
@@ -89,7 +89,7 @@ describe("Product form create tests", () => {
 
   test("should clear validation messages when filling out the form correctly", async () => {
 
-    render (
+    render(
       <Router history={history}>
         <Form />
       </Router>
@@ -109,12 +109,12 @@ describe("Product form create tests", () => {
     const imgUrlInput = screen.getByTestId("imgUrl");
     const descriptionInput = screen.getByTestId("description");
     const categoriesInput = screen.getByLabelText("Categorias");
-    
+
     await selectEvent.select(categoriesInput, ["Eletrônicos", "Computadores"]);
     userEvent.type(nameInput, "Mouse Razer");
     userEvent.type(priceInput, "277,90");
     userEvent.type(imgUrlInput, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
-    userEvent.type(descriptionInput, "Mouse braaabo");      
+    userEvent.type(descriptionInput, "Mouse braaabo");
 
     await waitFor(() => {
       const messages = screen.queryAllByText("Campo obrigatório");
@@ -122,4 +122,63 @@ describe("Product form create tests", () => {
     });
 
   })
+});
+
+describe("Product form update tests", () => {
+
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({
+      productId: '2'
+    })
+  });
+
+  test("should load product info when opening page", async () => {
+
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    )
+
+    const nameInput = screen.getByTestId("name");
+    const priceInput = screen.getByTestId("price");
+    const imgUrlInput = screen.getByTestId("imgUrl");
+    const descriptionInput = screen.getByTestId("description");
+
+    await waitFor(() => {
+      expect(nameInput).toHaveValue(productResponse.name);
+      expect(priceInput).toHaveValue(String(productResponse.price).replace('.', ','));
+      expect(imgUrlInput).toHaveValue(productResponse.imgUrl);
+      expect(descriptionInput).toHaveValue(productResponse.description);
+    });
+
+  });
+
+  test("should show toast and redirect to Product List when subscribed", async () => {
+
+    render(
+      <Router history={history}>
+        <ToastContainer />
+        <Form />
+      </Router>
+    )
+
+    const descriptionInput = screen.getByTestId("description");
+
+    await waitFor(() => {
+      expect(descriptionInput).toHaveValue(productResponse.description);
+    });
+
+    const submitButton = screen.getByRole("button", { name: /salvar/i });
+
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText("Produto cadastrado com sucesso")).toBeInTheDocument();
+    });
+
+    expect(history.location.pathname).toEqual("/admin/products");
+
+  });
+
 });
